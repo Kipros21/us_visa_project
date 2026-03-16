1ac5239de0958742cafece58f1230ec0fa234698
Cristhian Huanqui <crist.hta21@gmail.com>
2026-03-15 19:05:59 -0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="1.0.0",
+    author="Kipros21",
+    description="A machine learning project for US visa prediction",
+    packages=find_packages(),
+)
